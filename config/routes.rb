93643db7c345f58509bff3ccc913e51468c06103
uavpliyc <uavpliyc@gamil.com>
2021-02-27 03:28:15 +0000

    [make_outline]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'home/top'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "home#top"
+  resource :user, except: [:new, :create, :destroy]
 end
